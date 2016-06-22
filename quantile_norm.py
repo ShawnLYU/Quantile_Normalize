69999c3ce1842a4cc31e73a7028f919b91d07c76
shawn <shawn_00852@hotmail.com>
2016-06-22 09:29:17 +0800

    Initialize the package

---
+++ quantile_norm.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+def quantileNormalize(df_input):
+    df = df_input.copy()
+    #compute rank
+    dic = {}
+    for col in df:
+        dic.update({col : sorted(df[col])})
+    sorted_df = pd.DataFrame(dic)
+    rank = sorted_df.mean(axis = 1).tolist()
+    #sort
+    for col in df:
+        t = np.searchsorted(np.sort(df[col]), df[col])
+        df[col] = [rank[i] for i in t]
+    return df
